41d6a21a0915ea6efded6ecfd4b6b3e190377516
fleevo32 <bmoney456headcrack@gmail.com>
2022-03-23 01:24:45 -0400

    refactor: start of ox_inv / ESX conversion

---
+++ server.lua
@@ -0,0 +1,23 @@
+ESX.RegisterUsableItem('stickynote', function(source, item)
+	local xPlayer = ESX.GetPlayerFromId(source)		
+	local itemData = exports.ox_inventory:Search(source, 'slots', 'stickynote')
+	for _, v in pairs(itemData) do
+		if v.metadata ~= nil then 
+			TriggerClientEvent('qpstickynote:client:ShowStickyNote', source, v.metadata.description, v.slot)
+		else
+			TriggerClientEvent('qpstickynote:client:ShowStickyNote', source, 'Leave a note here...', v.slot)
+		end
+	end
+end)
+
+RegisterServerEvent("qpr:server:StickyNoteWordChange")
+AddEventHandler("qpr:server:StickyNoteWordChange", function(text, slot)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local stickynote = exports.ox_inventory:Search(xPlayer.source, 'slots', 'stickynote')
+	for k, v in pairs(stickynote) do
+		stickynote = v
+	end
+	stickynote.metadata.description = text
+	exports.ox_inventory:SetMetadata(xPlayer.source, stickynote.slot, stickynote.metadata)
+end)
+
