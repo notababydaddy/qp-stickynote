44b3c7cf7382110e8d33da603ed4232c34e7bf98
fleevo32 <bmoney456headcrack@gmail.com>
2022-03-23 01:27:45 -0400

    chore(fxmanifest)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,26 @@
-fx_version 'cerulean'
-game 'gta5'
+-- FX Information --
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
 
-ui_page 'html/index.html'
+-- Resource Information --
+name         'qpstickynote'
+author       'qpr'
+version      '1.0.1'
+repository   'https://github.com/ohqpr/qpstickynote'
+description  'Sticky notepad'
+
+-- Manifest --
+dependencies {
+  'es_extended',
+  'ox_inventory'
+}
+
+ui_page       'html/index.html'
+shared_script '@es_extended/imports.lua'
+client_script 'client.lua'
+server_script 'server.lua
 
 files {
-    'html/**'
-}
-
-client_scripts {
-    'config.lua',
-    'client/main.lua',
-}
-
-server_scripts {
-    'config.lua',
-    'server/main.lua',
+ 'html/**'
 }
